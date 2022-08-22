5de242067a43298140495ea37f05415b9c3e49a9
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-08-22 20:10:31 -0300

    Subida de servidor y subida de Entrega complementaria 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const Metodopago= "Cripto"
+
+switch (Metodopago){
+    case "Efectivo":
+    console.log("Pagaste en efectivo")
+    break;
+    case "Cheque":
+    console.log("Pagaste con un cheque");
+    brack;
+    case "Cripto":
+    console.log("Pagaste con cripto")
+    default:
+        console.log("Metodo de pago no aceptado")
+        break;}
