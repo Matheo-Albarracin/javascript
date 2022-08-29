4dda967f40421f359dd5b71c31791f8e9558443a
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-08-29 18:08:45 -0300

    añadi un ciclo con switch

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-let entrada = prompt("Ingrese un texto o ESC para interrumpir")
+let opciones = prompt(`Ingrese un Numero para comprar:
+1: "Remeras";
+2: "Zapatillas"
+3: "Pantalones"
+4: "Buzos"
+`);
 
-let texto = "texto";
+    switch (opciones){
+    case "1":{;
+        console.log("Remeras");
+        break
+    }
+    case "2":{
+        console.log("Zapatillas");
+        break
+    }
+    case "3":{
+        console.log("Pantalones");
+        break
+    }
 
-while (entrada != "ESC" && entrada != "esc"){
-    texto + entrada + " "
-    entrada = prompt("Ingrese un texto o ESC para interrumpir");
-    if (texto === ESC){
-        continue
+    case "4":{
+        console.log("Buzos");
+        break;
+    }
+
+    default: {
+        console.log("No ingresaste una compra correctamente");
+        break
     }
 }
