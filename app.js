8426fae2ce9719722dbebcf865f5252e1e4fa2db
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-09-07 19:59:45 -0300

    Añadi arrays y functions al proyecto

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
-let opciones = prompt(`Ingrese un Numero para comprar:
-1: "Remeras";
-2: "Zapatillas"
-3: "Pantalones"
-4: "Buzos"
-`);
+class Producto{
+    constructor(marca, valor){
+        this.marca = marca;
+        this.valor= valor;
+    }
+}
 
-    switch (opciones){
-    case "1":{;
-        console.log("Remeras");
-        break
-    }
-    case "2":{
-        console.log("Zapatillas");
-        break
-    }
-    case "3":{
-        console.log("Pantalones");
-        break
-    }
+let opciones = [
+    {marca: "Remeras",valor:"10500"},
+    {marca: "Pantalones",valor:"21250"},
+    {marca: "Zapatillas",valor:"8000"},
+    {marca: "Buzos",valor:"15500"},
+];
 
-    case "4":{
-        console.log("Buzos");
-        break;
-    }
+function cargarProducto(arrayProductos){
+    let marca = prompt("ingrese el producto seleccionado");
+    let valor = parseInt(prompt("ingrese el precio"));
+    const  nuevoProducto = new Producto(marca, valor,);
+    arrayProductos.push(nuevoProducto);
+    console.log("producto añadido al carrito");
+}
 
-    default: {
-        console.log("No ingresaste una compra correctamente");
-        break
+function mostrarProductos(){
+    arrayProductos.forEach((producto)=>{
+        alert(`usted eligio un/unos ${producto.marca} por un precio de ${producto.valor}`)
+    })
+}
+
+
+function finalizarCompra(){
+    const total = arrayProductos.reduce ((acc, el)=> acc + el.valor, 0);
+    alert (`gracias por su compra! el total a pagar es de ${total}`);
+}
+
+let arrayProductos = [];
+
+let opcion = prompt(
+    " Ingrese una opcion: \n 1: Buzos \n 2: Remeras \n 3: Buzos \n 4: Zapatillas \n 5: Mostrar Carrito \n 6: Salir"
+);
+
+while ( opcion !== "6"){
+    if (opcion === "1"){
+        cargarProducto(arrayProductos);
     }
+    if (opcion === "2"){
+        cargarProducto(arrayProductos);
+    }
+    if (opcion === "3"){
+        cargarProducto(arrayProductos);
+    }
+    if (opcion === "4"){
+        cargarProducto(arrayProductos);
+    }
+    if (opcion === "5"){
+        mostrarProductos(arrayProductos);
+    }
+    if (opcion === "6"){
+        finalizarCompra(arrayProductos);
+    }
+    opcion = prompt (
+        "Vuelva a ingresar una opcion: \n 1: Buzos \n 2: Remeras  \n 3: Buzos \n 4: Zapatillas \n 5: Mostrar Carrito \n 6: Salir"
+    );
 }
