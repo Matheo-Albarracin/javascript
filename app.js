e7e8857828616ceaa061b96e1e5926d812ba630c
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-09-12 19:41:06 -0300

    agrego dom y eventos al proyecto

--- app.js
+++ app.js
@@ -1,63 +1,46 @@
-class Producto{
-    constructor(marca, valor){
-        this.marca = marca;
-        this.valor= valor;
-    }
-}
-
-let opciones = [
-    {marca: "Remeras",valor:"10500"},
-    {marca: "Pantalones",valor:"21250"},
-    {marca: "Zapatillas",valor:"8000"},
-    {marca: "Buzos",valor:"15500"},
+let productos = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        precio: 12500,
+        imagen: "./imagenes/a1.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        precio: 12800,
+        imagen: "./imagenes/a2.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        precio: 13500,
+        imagen: "./imagenes/a3.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Producto 4",
+        precio: 15000,
+        imagen: "./imagenes/a4.jpg"
+    },
 ];
 
-function cargarProducto(arrayProductos){
-    let marca = prompt("ingrese el producto seleccionado");
-    let valor = parseInt(prompt("ingrese el precio"));
-    const  nuevoProducto = new Producto(marca, valor,);
-    arrayProductos.push(nuevoProducto);
-    console.log("producto añadido al carrito");
+const dibujarProductos = () => {
+    let contenedor = document.getElementsById("container");
+    Productos.forEach((producto,indice) =>{
+    let card= document.createElement("div");
+    card.classList.add("card" , "col-sm-12" , "col-lg-3");
+    card.innerHTML = `<img src="${producto.imagen}" class="card-img-top" alt="">
+    <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.precio}</p>
+        <a href="#" class="btn btn-info" onClick="agregarAlCarrito()"  >Comprar</a>
+    </div>`;
+    contenedor.appendChild(card)
+    });
 }
-
-function mostrarProductos(){
-    arrayProductos.forEach((producto)=>{
-        alert(`usted eligio un/unos ${producto.marca} por un precio de ${producto.valor}`)
-    })
-}
-
-
-function finalizarCompra(){
-    const total = arrayProductos.reduce ((acc, el)=> acc + el.valor, 0);
-    alert (`gracias por su compra! el total a pagar es de ${total}`);
-}
-
-let arrayProductos = [];
-
-let opcion = prompt(
-    " Ingrese una opcion: \n 1: Buzos \n 2: Remeras \n 3: Buzos \n 4: Zapatillas \n 5: Mostrar Carrito \n 6: Salir"
-);
-
-while ( opcion !== "6"){
-    if (opcion === "1"){
-        cargarProducto(arrayProductos);
-    }
-    if (opcion === "2"){
-        cargarProducto(arrayProductos);
-    }
-    if (opcion === "3"){
-        cargarProducto(arrayProductos);
-    }
-    if (opcion === "4"){
-        cargarProducto(arrayProductos);
-    }
-    if (opcion === "5"){
-        mostrarProductos(arrayProductos);
-    }
-    if (opcion === "6"){
-        finalizarCompra(arrayProductos);
-    }
-    opcion = prompt (
-        "Vuelva a ingresar una opcion: \n 1: Buzos \n 2: Remeras  \n 3: Buzos \n 4: Zapatillas \n 5: Mostrar Carrito \n 6: Salir"
-    );
-}
+    dibujarProductos();
+    
+    const agregarAlCarrito = () => {
+        alert("Producto Agregado Al Carrito Correctamente");
+    };
