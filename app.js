0e72eb072906149d759adbe4501d8905f668b3f0
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-09-19 18:45:43 -0300

    agregue un alert a comprar

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
 let productos = [
-    {
-        id: 1,
-        nombre: "Producto 1",
-        precio: 12500,
-        imagen: "./imagenes/a1.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Producto 2",
-        precio: 12800,
-        imagen: "./imagenes/a2.jpg"
-    },
-    {
-        id: 3,
-        nombre: "Producto 3",
-        precio: 13500,
-        imagen: "./imagenes/a3.jpg"
-    },
-    {
-        id: 4,
-        nombre: "Producto 4",
-        precio: 15000,
-        imagen: "./imagenes/a4.jpg"
-    },
+    {id: "1", nombre: "Zapatillas Rojas", precio: 15000, img: "./imagenes/a1.jpg"},
+    {id: "2", nombre: "Zapatillas Adidas", precio: 12500, img: "./imagenes/a2.jpg"},
+    {id: "3", nombre: "Zapatillas Negras", precio: 13500, img: "./imagenes/a3.jpg"},
+    {id: "4", nombre: "Zapatillas Rojas", precio: 10500, img: "./imagenes/a4.jpg"},
 ];
 
-const dibujarProductos = () => {
-    let contenedor = document.getElementsById("container");
-    Productos.forEach((producto,indice) =>{
-    let card= document.createElement("div");
-    card.classList.add("card" , "col-sm-12" , "col-lg-3");
-    card.innerHTML = `<img src="${producto.imagen}" class="card-img-top" alt="">
-    <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">${producto.precio}</p>
-        <a href="#" class="btn btn-info" onClick="agregarAlCarrito()"  >Comprar</a>
-    </div>`;
-    contenedor.appendChild(card)
-    });
-}
-    dibujarProductos();
-    
-    const agregarAlCarrito = () => {
-        alert("Producto Agregado Al Carrito Correctamente");
-    };
+const recorrerProductos = ()=> {
+    let contenedor = document.getElementById("container")
+    productos.forEach((producto,indice)=>{
+        let card = document.createElement("div")
+        card.classList.add("card", "col-sm-8", "col-lg-3",);
+        card.innerHTML = `<img src="${producto.img}"" class="card-img-top" alt="...">
+        <h5 class="card-title">"${producto.nombre}"</h5>
+        <div class="card-body">
+        <p class="card-text">"${producto.precio}"</p>
+        <a href="#" class="btn btn-primary" onClick="seleccionarAlCarrito()">Comprar</a>
+        </div>`;
+        contenedor.appendChild(card)
+        })
+    }
+
+    recorrerProductos();
+
+    const seleccionarAlCarrito = () => {
+        alert("producto seleccionado al carrito")
+    }
