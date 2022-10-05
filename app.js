88ff1e63c1784e14ffa438481cb8e5617b54cdb3
Matheo Albarracin <matheoalbarra@hotmail.com>
2022-10-05 19:45:54 -0300

    agregue Fetch al proyecto

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-let productos = [
-    {id: "1", nombre: "Zapatillas Rojas", precio: 15000, img: "./imagenes/a1.jpg"},
-    {id: "2", nombre: "Zapatillas Adidas", precio: 12500, img: "./imagenes/a2.jpg"},
-    {id: "3", nombre: "Zapatillas Negras", precio: 13500, img: "./imagenes/a3.jpg"},
-    {id: "4", nombre: "Zapatillas Rojas", precio: 10500, img: "./imagenes/a4.jpg"},
-];
-
-const guardarProductos = (clave, valor) => { localStorage.setItem(clave, valor)}
-for (const producto of productos) {
-    guardarProductos (producto.id, JSON.stringify(producto))
-}
-
 const recorrerProductos = ()=> {
     let contenedor = document.getElementById("container")
+    fetch("/data.json")
+    .then((res)=>res.json())
+    .then((productos)=>{
     productos.forEach((producto,indice)=>{
         let card = document.createElement("div")
         card.classList.add("card", "col-sm-8", "col-lg-3",);
-        card.innerHTML = `<img src="${producto.img}"" class="card-img-top" alt="...">
-        <h5 class="card-title">"${producto.nombre}"</h5>
+        card.innerHTML = `<img src="${producto.imagen}"" class="card-img-top" alt="...">
+        <h5 class="card-title">"${producto.tag}"</h5>
         <div class="card-body">
         <p class="card-text">"${producto.precio}"</p>
         <a href="#" class="btn btn-primary" id = "boton" onClick="agregarAlCarrito(${indice})">Comprar</a>
         </div>`;
         contenedor.appendChild(card)
         })
-    }
+})
+}
 
     recorrerProductos();
     let carrito = [];
